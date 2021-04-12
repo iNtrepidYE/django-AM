13a73ed153945c93b4c55534b1497fa3106ed610
curry <lin.721921745330@gmail.com>
2021-04-12 10:35:07 +0800

    XXXX

--- AMproject/wsgi.py
+++ AMproject/wsgi.py
@@ -1,6 +1,5 @@
 import os
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AMproejct.settings")
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
